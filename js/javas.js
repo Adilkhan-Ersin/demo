7d49057b2ecd1c3a95094d28e399a562d1aa94ed
Adilkhan Ersin <ersinadilhan.51@gmail.com>
2024-06-26 16:43:53 +0500

    Mobile Responsiveness added :)

--- js/javas.js
+++ js/javas.js
@@ -21,6 +21,53 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
   
+  //Nav-bar
+  function navbar() {
+    let activeItemIndicator = CSSRulePlugin.getRule(".menu-item p#active::after");
+    const toggleButton = document.querySelector(".burger");
+    let isOpen = false;
+  
+    gsap.set(".menu-item p", { y: 225 });
+  
+    const timeline = gsap.timeline({ paused: true });
+  
+    timeline.to(".overlay", {
+      duration: 1.5,
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      ease: "power4.inOut"
+    });
+  
+    timeline.to(".menu-item p", {
+      duration: 1.5,
+      y: 0,
+      stagger: 0.2,
+      ease: "power4.out"
+    }, "-=1");
+  
+    timeline.to(activeItemIndicator, {
+      width: "100%",
+      duration: 1,
+      ease: "power4.out",
+      delay: 0.5
+    }, "<");
+      
+    timeline.to(".sub-nav", {
+      bottom: "10%",
+      opacity: 1,
+      duration: 0.5,
+      delay: 0.5
+    }, "<");
+  
+    toggleButton.addEventListener("click", function() {
+      if (isOpen) {
+        timeline.reverse();
+      } else {
+        timeline.play();
+      }
+      isOpen = !isOpen;
+    })
+  }
+  navbar();
   //header
   function header() {
     gsap.to('.title_paralax', {
@@ -228,4 +275,4 @@ document.addEventListener("DOMContentLoaded", function () {
 
   }
   cursor()
-});
+})
